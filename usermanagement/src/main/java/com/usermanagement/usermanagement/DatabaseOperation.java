8fb16599bfa91a5fca78f62d93c2a980fd89ced7
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-05 00:29:20 +0200

    db

--- usermanagement/src/main/java/com/usermanagement/usermanagement/DatabaseOperation.java
+++ usermanagement/src/main/java/com/usermanagement/usermanagement/DatabaseOperation.java
@@ -3,8 +3,6 @@ package com.usermanagement.usermanagement;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
-import com.mongodb.ServerApi;
-import com.mongodb.ServerApiVersion;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
