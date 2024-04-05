d12ff846946078d1efce7a2f9a6acb1a0584072b
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-05 23:24:45 +0200

    java

--- usermanagement/src/main/java/com/usermanagement/usermanagement/Routes.java
+++ usermanagement/src/main/java/com/usermanagement/usermanagement/Routes.java
@@ -3,40 +3,64 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.io.Resource;
 import java.nio.charset.StandardCharsets;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StreamUtils;
 import java.io.IOException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class Routes {
+
+    @Autowired
+    private UserRepository userRepository; 
+
+    // THIS ENABLES US TO HANDLE GET REQUESTS
 
     @GetMapping("/")
     public String LoginPath() throws IOException {
         Resource resource = new ClassPathResource("static/publicFiles/login.html");
 
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
 
-       String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
-
-       return htmlContent;
+        return htmlContent;
     }
 
     @GetMapping("/login")
     public String HomePath() throws IOException {
         Resource resource = new ClassPathResource("static/publicFiles/login.html");
 
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
 
-       String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
-
-       return htmlContent;
+        return htmlContent;
     }
 
     @GetMapping("/signup")
     public String SignupPath() throws IOException {
-       Resource resource = new ClassPathResource("static/publicFiles/signup.html");
+        Resource resource = new ClassPathResource("static/publicFiles/signup.html");
 
-       String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
 
-       return htmlContent; 
+        return htmlContent; 
     }
+
+    // POST REQUESTS    
+
+    @PostMapping("/signup")
+    public String postMethodName(@RequestParam String picture,@RequestParam String name,@RequestParam String lastName,@RequestParam String email, @RequestParam String password) {
+        User user = new User();
+        user.setPicture(picture);
+        user.setName(name);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        userRepository.save(user);
+        return "Received POST request for signup with email: " + email + " and password: " + password;
+    }
+    
     
 }
--- usermanagement/src/main/java/com/usermanagement/usermanagement/UsermanagementApplication.java
+++ usermanagement/src/main/java/com/usermanagement/usermanagement/UsermanagementApplication.java
@@ -12,6 +12,7 @@ public class UsermanagementApplication {
 		DatabaseOperation databaseOperation = new DatabaseOperation();
 
 		databaseOperation.getClass();
+
 	}
 
 }
